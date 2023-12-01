35375a62ef820ca08f27104010d85c537cd8d14c
mohamedhamss <midosun@gmail.com>
2023-12-01 15:36:01 +0200

    upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-center'> Welcome </h1>
+
+      <h1 className="hager"> Welcome </h1>
+
+      <h1> Welcome </h1>
+    </div>
+  )
+}
+
+export default App
